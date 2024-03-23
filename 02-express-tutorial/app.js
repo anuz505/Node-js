466efb1ce5251f0d461c3fc349948ac2723c8370
anuj bhandari <anujb364@gmail.com>
2024-03-23 13:22:21 +0545

    new commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+const express =  require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    console.log('user hit the response');
+    res.send('home page')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('about page')
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resource is not found</h1>')
+})
+
+app.listen(5000,()=>{
+    console.log('server is listening');
+})
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
