21b63f8dfd1395bfb0dd9b42e19cad8d789a5e3d
anuj bhandari <anujb364@gmail.com>
2024-03-25 14:24:01 +0545

    new commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,54 @@
-const express =  require('express')
+const express = require('express')
 const app = express()
+const {products} = require('./data.js')
 
 app.get('/',(req,res)=>{
-    console.log('user hit the response');
-    res.send('home page')
+    res.send(`<h1> Home Page</h1><a href="/api/products">products</a>`)
 })
 
-app.get('/about',(req,res)=>{
-    res.send('about page')
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product;
+        return{id,name,image}
+    })
+    res.json(newProducts )
 })
 
-app.all('*',(req,res)=>{
-    res.status(404).send('<h1>resource is not found</h1>')
+app.get('/api/products/:productID',(req,res)=>{
+    console.log(req.params);
+    const {productID} = req.params;
+    const singleProduct = products.find((product)=>product.id ===Number(productID))
+    if(!singleProduct){
+        return res.status(404).send('Prodcut Does Not Exist')
+    }
+    console.log(singleProduct);
+    res.json(singleProduct )
+})
+
+app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
+    console.log(req.params)
+    res.send('hello world')
+})
+
+app.get('/api/v1/query',(req,res)=>{
+  /*   console.log(req.query); */
+  const {search,limit} = req.query
+    let sortedProducts = [...products];
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search);
+        })
+    }
+    if(limit){
+        return sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.length<1){
+        return res.status(200).json({sucess:true,data:[]})
+    }
+    res.status(200).json(sortedProducts)
 })
 
 app.listen(5000,()=>{
-    console.log('server is listening');
+    console.log('server is listening to port 5000.....');
 })
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
